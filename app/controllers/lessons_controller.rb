c3ef8842584c3a8de34662a25dafc1eecadbc13d
kareemrady <kareem_rady@yahoo.com>
2016-01-17 23:47:59 +0000

    Only enrolled users can see lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :user_enrolled_in_course?
+	
 	def show
 	end
 
@@ -6,8 +9,19 @@ class LessonsController < ApplicationController
 	private
 
 	helper_method :current_lesson
-
+	helper_method :user_enrolled_in_course?
+		
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+	
+	def user_enrolled_in_course?
+		course = current_lesson.section.course
+		condition =  current_user.enrolled_in?(course)
+		if (condition)
+		 return true
+		else
+		 redirect_to course_path(course), alert: 'You need to Enroll in Course'
+		end
 	end
 end
