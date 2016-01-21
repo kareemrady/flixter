7eb72191cd701b3ef0275ab7dc03b47610f4fd43
Kareem <kareem_rady@yahoo.com>
2016-01-21 04:21:49 +0000

    Added dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Flixter::Application.routes.draw do
   devise_for :users
+  resource :dashboard, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
