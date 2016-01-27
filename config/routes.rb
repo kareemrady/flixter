fded8ced641d08d6a837b1abebe034450cae756f
kareem rady <kareem_rady@yahoo.com>
2016-01-27 04:56:11 +0000

    Added Privacy static page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Flixter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'static_pages#index'
+   get 'privacy', to: 'static_pages#privacy'
 
    resources :courses, only: [:index, :show] do
      resources :enrollments, only: :create
